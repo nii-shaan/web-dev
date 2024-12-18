1b4634773d5cc2dd8dc5489a98032e9dfc2e0ec8
Nishan Bista <nishanbista525@gmail.com>
2024-12-18 21:19:10 +0545

    buttons

--- DOM_manipulation/index.js
+++ DOM_manipulation/index.js
@@ -1,9 +1,13 @@
-const head = document.getElementById("heading");
+const head = document.getElementById("heading")
 console.log(head);
-const paragraph = document.getElementById("paragraph");
 
-const highlightButton = document.querySelector("#highlight");
-const toggleClass = document.querySelector("#toggle-class-button");
+
+
+
+const paragraph = document.getElementById("paragraph")
+
+const highlightButton = document.querySelector("#highlight")
+const toggleClass = document.querySelector("#toggle-class-button")
 const addItem = document.querySelector("#add-list-item");
 const removeItem = document.querySelector("#remove-list-item");
 const doSomething = document.querySelector("#doSomething");
@@ -13,34 +17,34 @@ console.log(addItem);
 const lists = document.querySelector("#lists");
 
 highlightButton.addEventListener("click", () => {
-  head.classList.add("highlighted");
-  paragraph.classList.add("highlighted");
+    head.classList.add("highlighted");
+    paragraph.classList.add("highlighted");
 });
 
 toggleClass.addEventListener("click", () => {
-  head.classList.remove("highlighted");
-  paragraph.classList.remove("highlighted");
+    head.classList.remove("highlighted");
+    paragraph.classList.remove("highlighted");
 });
 
 addItem.addEventListener("click", () => {
-  const item = document.createElement("li");
-  item.textContent = `Item ${lists.childElementCount + 1}`;
-  item.classList.add("list-item");
-  lists.appendChild(item);
+    const item = document.createElement("li");
+    item.textContent = `Item ${lists.childElementCount + 1}`;
+    item.classList.add("list-item");
+    lists.appendChild(item);
 });
 
 removeItem.addEventListener("click", () => {
-  const item = lists.lastChild;
-  if (item) item.remove();
+    const item = lists.lastChild;
+    if (item) item.remove();
 });
 
 doSomething.addEventListener("click", () => {
-  const listItems = document.querySelectorAll(".list-item");
+    const listItems = document.querySelectorAll(".list-item");
 
-  for (let i = 0; i < listItems.length; i++) {
-    let item = listItems[i];
-    item.style.color = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(
-      Math.random() * 256
-    )},${Math.floor(Math.random() * 256)})`;
-  }
+    for (let i = 0; i < listItems.length; i++) {
+        let item = listItems[i];
+        item.style.color = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(
+            Math.random() * 256
+        )},${Math.floor(Math.random() * 256)})`;
+    }
 });
--- login-signup/script.js
+++ login-signup/script.js
@@ -1,11 +1,18 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
 
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const form = document.getElementById('form');
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = JSON.parse(localStorage.getItem("email"));
+  const password = JSON.parse(localStorage.getItem("password"));
 
+  if (!email || !password) {
+    alert("login failed");
+  } else {
+    localStorage.setItem("email", JSON.stringify(email.value));
+    localStorage.setItem("password", JSON.stringify(password.value));
+  }
+  
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(username.value);
-    console.log(password.value);
 });
